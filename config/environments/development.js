794cbe74b9fd3dc49b865412cc56a0fa2ee48195
Hack Reactor Students <github@hackreactor.com>
2013-10-03 21:29:51 -0700

    defined user model for DB

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,32 +1,39 @@
 var express = require('express');
 var path = require('path');
 var mongoose = require('mongoose');
+var fs = require('fs');
 
 module.exports = function (app) {
-    app.configure('development', function () {
-        app.use(function staticsPlaceholder(req, res, next) {
-            return next();
-        });
-
-        app.set('port', process.env.PORT || 9000);
-        app.set('views', path.join(app.directory, '/app'));
-        app.engine('html', require('ejs').renderFile);
-        app.set('view engine', 'html');
-        app.use(express.favicon());
-        app.use(express.logger('dev'));
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
-        app.use(express.cookieParser('your secret here'));
-        app.use(express.session());
-
-        app.use(function middlewarePlaceholder(req, res, next) {
-          return next();
-        });
-
-        app.use(app.router);
-        app.use(express.errorHandler());
+  app.configure('development', function () {
+    app.use(function staticsPlaceholder(req, res, next) {
+        return next();
     });
+
+    app.set('port', process.env.PORT || 9000);
+    app.set('views', path.join(app.directory, '/app'));
+    app.engine('html', require('ejs').renderFile);
+    app.set('view engine', 'html');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+
+    app.use(function middlewarePlaceholder(req, res, next) {
+      return next();
+    });
+
+    app.use(app.router);
+    app.use(express.errorHandler());
+
     var db = mongoose.connect('mongodb://localhost/meetmeDev');
+    var modelsPath = path.normalize(__dirname) + '/server/models';
+    fs.readdirSync(modelsPath).forEach(function (file) {
+      require(modelsPath + '/' + file);
+    });
+  });
+
 };
 
 
