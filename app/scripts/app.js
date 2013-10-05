4a30f6d75f4e47977e6257cc32511edc51596554
Matthew Goo <mattgoo@gmail.com>
2013-10-05 10:05:57 -0700

    fixed no meetmeApp bug, and schema bug

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
-angular.module('meetmeApp')
+angular.module('meetmeApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- server/models/event.js
+++ server/models/event.js
@@ -1,3 +1,6 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
 var EventSchema = new Schema ({
   name: String,
   description: String,
