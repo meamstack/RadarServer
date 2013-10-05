f66630f17b04ef4a390175620cb1f3ee4e773600
Matthew Goo <mattgoo@gmail.com>
2013-10-04 21:18:21 -0700

    code cleanup. created passport.js file

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-var express = require('express'),
-    // routes = require('./routes'),
-    path = require('path');
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
 
 var app = express();
 app.directory = __dirname;
 
 require('./config/environments')(app);
+var mongoose = require('mongoose');
+
+// mongoose connect
+mongoose.connect('mongodb://localhost/meetmeDev');
+
+var modelsPath = path.join(app.directory, '/server/models');
+fs.readdirSync(modelsPath).forEach(function (file) {
+  require(modelsPath + '/' + file);
+});
+
+require(path.join(app.directory, '/config/environments/passport.js'));
+
 require('./routes')(app);
 
 module.exports = app;
--- config/environments/development.js
+++ config/environments/development.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var path = require('path');
-var mongoose = require('mongoose');
-var fs = require('fs');
 var passport = require('passport');
-var FacebookStrategy = require('passport-facebook').Strategy;
 
 module.exports = function (app) {
   app.configure('development', function () {
@@ -31,65 +28,5 @@ module.exports = function (app) {
 
     app.use(app.router);
     app.use(express.errorHandler());
-
-    mongoose.connect('mongodb://localhost/meetmeDev');
-    var modelsPath = path.join(app.directory, '/server/models');
-    fs.readdirSync(modelsPath).forEach(function (file) {
-      require(modelsPath + '/' + file);
-    });
   });
-
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
-
-  passport.deserializeUser(function(id, done) {
-      User.findOne({
-        _id: id
-      }, function(err, user) {
-        done(err, user);
-      });
-  });
-
-  var User = mongoose.model('User');
-
-  passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_MEETME_APIKEY,
-    clientSecret: process.env.FACEBOOK_MEETME_APPSECRET,
-    callbackURL: "http://meetme123.com:3000/auth/facebook/callback"
-    },
-    function(accessToken, refreshToken, profile, done) {
-      console.log('accessToken',accessToken)
-      User.findOne({'facebook.id': profile.id},function(err, user){
-        if(err) { return done(err); }
-        var newUser = new User({
-          name: profile.name,
-          email: profile.email,
-          gender: profile.gender
-        });
-
-        newUser.save(function(err) {
-          if(err) {
-            throw err;
-          }
-          return done(err, user);
-          // res.send('User saved successfully.');
-        });
-      });
-      // User.findOrCreate(function(err, user) {
-      //   if (err) { return done(err); }
-      //   done(null, user);
-      // });
-    }
-  ));
-
-
 };
-
-
-//passport session init
-
-//db
-//monogodb://localhost/collectionName
-
-//handle errors
