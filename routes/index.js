88b680256f879f0825f511107377d1c4176ea348
Matthew Goo <mattgoo@gmail.com>
2013-10-04 21:49:20 -0700

    added event and user schema for mongo

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,9 @@
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 var passport = require('passport');
-var permissions = [ 'email',
-                    'user_about_me',
-                    // 'user_relationship_details',
-                    'user_status',
-                    // 'user_website',
-                    'user_groups',
+var permissions = [ 'email'];//,
                     // 'user_photos',
-                    // 'user_relationships',
-                    'user_work_history',
-                    // 'user_hometown',
-                    'user_friends',
-                    'user_activities',
-                    // 'user_education_history',
-                    // 'user_religion_politics',
-                    'user_events',
-                    'friends_about_me',
-                    'friends_activities',
-                    'friends_birthday',
-                    'friends_interests',
-                    'friends_relationships',
-                    'friends_work_history',
-                    'friends_events',
-                    'user_actions.books',
-                    'user_actions.video',
-                    'user_actions.music',
-                    'friends_actions.news',
-                    'friends_education_history',
-                    // 'friends_religion_politics',
-                    // 'friends_subscriptions',
-                    'friends_status',
-                    // 'friends_hometown',
-                    // 'friends_photos',
-                    // 'friends_website',
-                    // 'user_videos',
-                    // 'friends_videos',
-                    // 'user_questions',
-                    // 'friends_questions',
-                    'user_birthday',
-                    // 'user_location',
-                    // 'friends_location',
-                    // 'user_games_activity',
-                    // 'friends_online_presence',
-                    // 'user_notes',
-                    // 'publish_actions',
-                    // 'user_subscriptions',
-                    'user_likes',
-                    'friends_likes'];
+                    // 'friends_photos',];
 
 
 module.exports = function (app) {
@@ -71,7 +27,6 @@ module.exports = function (app) {
         var newProfile = new User({
           name: info.name,
           email: info.email,
-          username: info.profile,
           gender: info.gender
         });
 
@@ -93,18 +48,5 @@ module.exports = function (app) {
 
   app.get('/auth/facebook/callback', passport.authenticate('facebook', {
     failureRedirect: '/'
-  }), function(req,res){ res.redirect('/'); }); 
-
-
-  // Redirect the user to Facebook for authentication.  When complete,
-  // Facebook will redirect the user back to the application at
-  //     /auth/facebook/callback
-  // app.get('/auth/facebook', passport.authenticate('facebook'));
-
-  // Facebook will redirect the user to this URL after approval.  Finish the
-  // authentication process by attempting to obtain an access token.  If
-  // access was granted, the user will be logged in.  Otherwise,
-  // authentication has failed.
-  // app.get('/auth/facebook/callback', 
-  // passport.authenticate('facebook', { failureRedirect: '/' }), function(req,res){ res.redirect('/'); });
+  }), function(req, res) { res.redirect('/'); });
 };
--- server/models/users.js
+++ server/models/users.js
@@ -3,9 +3,20 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema ({
   name: String,
-  email:String,
-  username:String,
-  gender:String
+  email: String,
+  gender: String,
+  eventsCreated: [Schema.Types.ObjectId],
+  eventsSaved: [Schema.Types.ObjectId]
+});
+
+var EventSchema = new Schema ({
+  name: String,
+  description: String,
+  location: [Number],
+  time: Date,
+  photo: Buffer,
+  activity: String
 });
 
 mongoose.model('User', UserSchema);
+mongoose.model('Event', EventSchema);
