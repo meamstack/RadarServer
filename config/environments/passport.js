6be5d4bc1b707607a42a3a81e0e0040421d74c03
Ed <eddyfh@gmail.com>
2013-10-12 20:12:23 -0700

    Line for running locally

--- config/environments/passport.js
+++ config/environments/passport.js
@@ -20,6 +20,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_MEETME_APIKEY,
   clientSecret: process.env.FACEBOOK_MEETME_APPSECRET,
+  // callbackURL: "http://meetme123.com:3000/auth/facebook/callback"
   callbackURL: "http://edhsieh.com/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
