63c26d004c4c0e30190986202583560a158f2f66
Matthew Goo <mattgoo@gmail.com>
2013-10-16 20:10:46 -0700

    threw all evt data into obj

--- test/spec/server.js
+++ test/spec/server.js
@@ -52,18 +52,23 @@ describe('facebook login', function () {
   });
 });
 
-describe('api/findEvents', function () {
-  var evt = {}
-  evt.testImage = fs.readFileSync(path.join(__dirname,'/hotAir_small.jpg'));
-  evt.loc = [37.800305,-122.409239];
-  evt.year = 2013;
-  evt.month = 11
-  evt.day = 31;
+describe('api event: create and find', function () {
+  var evt = {
+    testImage: fs.readFileSync(path.join(__dirname,'/hotAir_small.jpg')),
+    loc: [37.800305,-122.409239],
+    year: 2013,
+    month: 11,
+    day: 31,
+    name: 'new years eve ball!',
+    desc: 'end the year with a bang',
+    uid: 'abcdefghijklmn1234567'
+  };
   evt.date = new Date(evt.year, evt.month, evt.day);
   evt.date = evt.date.toISOString();
-  evt.name = 'new years eve ball!';
-  evt.desc = 'end the year with a bang';
-  evt.uid = 'abcdefghijklmn1234567';
+
+  var anotherEvt = {
+
+  };
 
   var options = JSON.stringify({
     location: evt.loc,
@@ -81,41 +86,82 @@ describe('api/findEvents', function () {
     userId: evt.uid
   });
 
-  beforeEach(function(done) {
-    server.Event.remove({}, function() {
+
+  describe('api/createEvent', function () {
+    it('should return a status code of 200', function (done) {
+      server.Event.remove({}, function () {
+        request(server.app)
+          .post('/api/createEvent')
+          .set('content-type', 'application/json')
+          .send(evtDetails)
+          .end(function(err, res) {
+            assert.equal(res.statusCode, 200);
+            done();
+          });
+      });
+    });
+/**** this test must happen before the next text, needs the event that is created ****/
+    it('should create an event', function (done) {
+      server.Event.remove({}, function () {
+        request(server.app)
+          .post('/api/createEvent')
+          .set('content-type', 'application/json')
+          .send(evtDetails)
+          .end(function(err, res) {
+            assert.equal(res.text, 'event create');
+            done();
+          });
+      });
+    });
+    it('should not create an event if it already exists', function (done) {
       request(server.app)
         .post('/api/createEvent')
         .set('content-type', 'application/json')
         .send(evtDetails)
         .end(function(err, res) {
+          assert.equal(res.text, 'already created');
           done();
         });
     });
   });
 
-  it('should return a status code of 200', function (done) {
-    request(server.app)
-      .post('/api/findEvents')
-      .set('content-type', 'application/json')
-      .send(options)
-      .end(function(err, res) {
-        assert.equal(res.statusCode, 200);
-        done();
-      });
-  });
-  it('should return the correct events', function (done) {
-    request(server.app)
-      .post('/api/findEvents')
-      .set('content-type', 'application/json')
-      .send(options)
-      .end(function(err, res) {
-        console.log(res);
-        // assert.equal(res[0].name, evt.name);
-        // assert.equal(res[0].userId, evt.uid);
-        // assert.equal(res[0].date, evt.date);
 
-        done();
+
+  describe('api/findEvents', function () {
+    beforeEach(function(done) {
+      server.Event.remove({}, function () {
+        request(server.app)
+          .post('/api/createEvent')
+          .set('content-type', 'application/json')
+          .send(evtDetails)
+          .end(function(err, res) {
+            done();
+          });
       });
+    });
+
+    it('should return a status code of 200', function (done) {
+      request(server.app)
+        .post('/api/findEvents')
+        .set('content-type', 'application/json')
+        .send(options)
+        .end(function(err, res) {
+          assert.equal(res.statusCode, 200);
+          done();
+        });
+    });
+    it('should return the correct events', function (done) {
+      request(server.app)
+        .post('/api/findEvents')
+        .set('content-type', 'application/json')
+        .send(options)
+        .end(function(err, res) {
+          var findEvt = JSON.parse(res.text)[0];
+          assert.equal(findEvt.name, evt.name);
+          assert.equal(findEvt.userId, evt.uid);
+          // assert.equal(findEvt.location, evt.loc);
+          done();
+        });
+    });
   });
 });
-
